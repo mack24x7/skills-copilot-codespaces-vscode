58baed97c974ae61879d3c4ceb8257afc803b5f0
Joe Mack <mack.joe@live.com>
2025-02-28 04:51:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 32,
+        skills: ['HTML', 'CSS', 'JS'],
+        salary: 25000
+    }
+    return member;
+}
